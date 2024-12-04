41fc10a3036547f618157e2827be673f6842cb05
Mahnoor Sheikh <67545978+mahnoorsheikh16@users.noreply.github.com>
2024-12-04 16:51:50 -0500

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -31,7 +31,7 @@ data.rename(columns={'PAY_0': 'PAY_1'}, inplace=True)
 
 #set page content
 if page == "📄Overview":
-    st.title('Credit Default Predictor: Identify Next Month’s High-Risk Clients')
+    st.header('Credit Default Predictor: Identify Next Month’s High-Risk Clients')
     st.write("")
     st.write("")
     st.write("Credit card defaults are causing significant losses for the Bank of Taiwan. This dashboard uses an advanced machine learning model to identify high-risk clients and predict defaults.")
