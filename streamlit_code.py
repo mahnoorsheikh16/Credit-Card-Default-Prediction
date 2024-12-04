342cd4ee72ad6127e34938f492ce6390fd9f7e66
Mahnoor Sheikh <67545978+mahnoorsheikh16@users.noreply.github.com>
2024-12-04 16:49:25 -0500

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -15,7 +15,7 @@ import joblib
 
 #add navigation sidebar
 st.sidebar.title("🔎Explore")
-page = st.sidebar.selectbox("Select a page:", ["🏡Home", "🕵🏻Identify Defaults", "📊Data Analysis & Insights", "🖥️Decoding the Algorithm"], index=0)
+page = st.sidebar.selectbox("Select a page:", ["Overview", "🕵🏻Identify Defaults", "📊Data Analysis & Insights", "🖥️Decoding the Algorithm"], index=0)
 for _ in range(15):  # Change 10 to the number of empty lines you want
     st.sidebar.write("")
 st.sidebar.write("View the code and dataset details: https://github.com/mahnoorsheikh16/Credit-Card-Default-Prediction")
